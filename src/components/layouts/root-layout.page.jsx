ee6c2d657b1dbdd2a18faf5bf8bea9d01e781fdd
yasisuru2005 <ydenagamaarachchi@gmail.com>
2025-10-23 20:30:49 +0530

    Fix authentication: Set user ID globally when user is logged in

--- src/components/layouts/root-layout.page.jsx
+++ src/components/layouts/root-layout.page.jsx
@@ -1,8 +1,21 @@
 import Navigation from "../Navigation";
 import { Outlet } from "react-router";
 import { Toaster } from "@/components/ui/sonner";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 function RootLayout() {
+  const { user, isLoaded } = useUser();
+
+  // Set user ID globally for API authentication
+  useEffect(() => {
+    if (isLoaded && user) {
+      localStorage.setItem("userId", user.id);
+    } else if (isLoaded && !user) {
+      localStorage.removeItem("userId");
+    }
+  }, [isLoaded, user]);
+
   return (
     <>
       <Navigation />
